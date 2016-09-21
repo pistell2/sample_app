8133ca1f0e5236ea5586161e3a2b95ac67b1fabd
John Pistella <pistell2@illinois.edu>
2016-09-21 12:36:42 -0500

    Added prototype for Destiny http get request

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,21 @@
+require 'net/http'
+require 'json'
+
 class StaticPagesController < ApplicationController
   def home
+  	# Break up to be only hostname & then list paths as
+  	uri = URI('https://www.bungie.net/Platform/Destiny/SearchDestinyPlayer/1/gmkryptonian/')
+  	
+  	Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
+  		req = Net::HTTP::Get.new(uri)
+  		req.add_field 'X-API-Key', '8e99962ee3bb4de498efcc507102286f'
+
+  		byebug
+  		response = http.request(req)
+  		# Handle Error (301 Redirect / 404 Page Not Found)
+  		body = JSON.parse(response.body)
+  		membershipId = body['Response'].first['membershipId']
+  	end
   end
 
   def help
