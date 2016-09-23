4e13feebc35d7afb3de6e74ecf96e354197fd84c
John Pistella <pistell2@illinois.edu>
2016-09-23 09:29:26 -0500

    Changed url to image-url in scss file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # Needed for local assets to be used in production
   config.serve_static_assets = true
   config.assets.compile = true
 end
